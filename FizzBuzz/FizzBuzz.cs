6ebbca58247b25e892b8448e75563318b4f369fd
David Jimenez <djimenez92@outlook.es>
2020-03-05 18:02:21 +0100

    Kata FizzBuzz to learn TDD

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+	public class FizzBuzz
+	{
+		public string Fizzbuzz(int number)
+		{
+			if (IsMultipleOfThree(number) && IsMultipleOfFive(number))
+				return "FizzBuzz";
+			if (IsMultipleOfFive(number))
+				return "Buzz";
+			if (IsMultipleOfThree(number))
+				return "Fizz";
+			return number.ToString();	
+		}
+
+		private bool IsMultipleOfThree(int numero)
+		{
+			return (numero % 3 == 0);
+		}
+
+		private bool IsMultipleOfFive(int numero)
+		{
+			return numero % 5 == 0;
+		}
+	}
+}
---
+++ FizzBuzzTests/FizzBuzzTests.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FizzBuzz;
+using System;
+namespace FizzBuzz.Tests
+{
+	[TestClass()]
+	public class FizzBuzzTests
+	{
+		public static FizzBuzz fizzbuzz;
+
+		[TestInitialize]
+		public void Setup()
+		{
+			fizzbuzz = new FizzBuzz();
+		}
+		[TestMethod()]
+		public void FizzBuzzReturnIntegerNumber()
+		{
+			var numero = 1;
+			var response = fizzbuzz.Fizzbuzz(numero);
+			Assert.AreEqual(numero.ToString(), response);
+		}
+
+		[TestMethod()]
+		public void FizzbuzzReturnFizzIfNumberIsThree()
+		{
+			var numero = 3;
+			var response = fizzbuzz.Fizzbuzz(numero);
+			Assert.AreEqual("Fizz", response);
+		}
+
+		[TestMethod()]
+		public void FizzbuzzReturnFizzIsNumberMultipleOfThree()
+		{
+			var numero = 6;
+			var response = fizzbuzz.Fizzbuzz(numero);
+			Assert.AreEqual("Fizz", response);
+		}
+
+		[TestMethod()]
+		public void FizzbuzzReturnBuzzIfNumberMultipleOfFive()
+		{
+			var numero = 10;
+			var fizzbuzz = new FizzBuzz();
+			var response = fizzbuzz.Fizzbuzz(numero);
+			Assert.AreEqual("Buzz", response);
+		}
+
+		[TestMethod()]
+		public void FizzbuzzReturnFizzBuzzIfNumberMultipleOfFiveAndThree()
+		{
+			var numero = 15;
+			var fizzbuzz = new FizzBuzz();
+			var response = fizzbuzz.Fizzbuzz(numero);
+			Assert.AreEqual("FizzBuzz", response);
+		}
+	}
+}
